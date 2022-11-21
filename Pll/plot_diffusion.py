9110fbee375f4f54a78c566567b1f2b6d6af420f
hansonTang <1591544223@qq.com>
2022-11-21 17:01:12 +0800

    add plot_diffusion

--- Pll/plot_diffusion.py
+++ Pll/plot_diffusion.py
@@ -6,8 +6,9 @@ import matplotlib.pyplot as plt
 def entrance():
     file_name = fetch_file_name()
     data = fetch_data_from_idx(file_name)
-    data_process(data) 
-
+    x, y = data_process(data) 
+    plot_diffusion(x,y,file_name[11:])
+# 取用户输入的idx文件名
 def fetch_file_name():
     help_msg = "python plot_diffusion.py -i [input file]"
     try:
@@ -18,35 +19,50 @@ def fetch_file_name():
         print(help_msg)
         exit()
 
+# 从用户输入的idx文件名取数据
 def fetch_data_from_idx(file_name):
     f = open(file_name,'r')
     raw_data = f.readlines()
     # print(raw_data)
-    formated_data = eval(raw_data)
-    # print(formated_data)
-    return formated_data
+    print(raw_data)
+    return raw_data
 
-def data_process(data):
+# 生成所有的x和y
+def data_process(raw_data):
     # print(data)
-    data[i]
+    # print(len(data))
+    x = []
+    y = []
+    for i in range(len(raw_data)):
+        print(eval(raw_data[i]))
+        x_new,y_new = generate_x_y(eval(raw_data[i]))
+        x.append(x_new)
+        y.append(y_new)
+    return x,y
+
+# 对每一个order对应的BFS生成x和y
+def generate_x_y(data):
     plot_data = {}
     key_list_data = list(data.keys())
-    print("*************************")
-    print("*************************")
-    print("*************************")
-    print("*************************")
-    print("*************************")
 
     for i in range(len(data)):
         plot_data[i] = data[key_list_data[i]]
     x = list(plot_data.keys())
     y = list(plot_data.values())
-    print(x)
-    print(y)
-    plt.scatter(x,y,s=1)
+    return x,y
+
+def plot_diffusion(x,y,file_name):
+    
+    # for i in range(len(x)):
+    #     plt.scatter(x[i],y[i],s=2)
+    plt.scatter(x[0],y[0],s=1,marker="+")
+    plt.scatter(x[1],y[1],s=1,marker="v")
+    plt.scatter(x[2],y[2],s=1,marker="^")
+    plt.scatter(x[3],y[3],s=1,marker="+")
+
+    plt.legend(['degree','closeness','betweenness','2-hop'])
+    plt.xlabel("x-th BFS")
+    plt.ylabel("Vertices")
+    plt.title(file_name)
     plt.show()
-        
-
-    # print(plot_data)
-
 entrance()
